1a0c0315b08e8d94f1bb70f2a161224894716e42
Phodal Huang <h@phodal.com>
2020-07-11 10:45:42 +0800

    refactor: fix entry open stream issue

--- src/classfile/class_loader.rs
+++ src/classfile/class_loader.rs
@@ -3,6 +3,23 @@ use crate::classfile::class_file_stream::ClassFileStream;
 use crate::oops::instanced_klass::InstanceKlass;
 use crate::classfile::class_loader_data::ClassLoaderData;
 
+pub struct ClassPathEntry {
+
+}
+
+impl ClassPathEntry {
+    pub fn new() -> ClassPathEntry {
+        ClassPathEntry {
+
+        }
+    }
+    pub fn open_stream(self, file_name: String) -> ClassFileStream {
+        ClassFileStream {
+
+        }
+    }
+}
+
 pub struct ClassLoader {
 
 }
@@ -10,8 +27,8 @@ pub struct ClassLoader {
 impl ClassLoader {
     pub fn load_class(&mut self, class_name: String) -> InstanceKlass {
         let file_name = self.file_name_for_class_name(class_name);
-        let mut stream = ClassFileStream::new();
-        stream.open_stream(file_name);
+        let mut entry = ClassPathEntry::new();
+        let stream = entry.open_stream(file_name);
 
         // let loader_data = ClassLoaderData::new();
         let klass = ClassFactory::create_from_stream(stream);
