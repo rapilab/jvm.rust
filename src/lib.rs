f3d095fe9cf2bca29205d023b98e0be7e8158303
Phodal Huang <h@phodal.com>
2020-07-11 12:18:11 +0800

    refactor: remove unused code

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,2 @@
 pub mod oops;
 pub mod classfile;
-pub mod util;
-
-pub mod klass_parser;
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,5 @@
-use std::fs::File;
-use std::io::Read;
-use byteorder::{ByteOrder, LittleEndian};
-use std::path::Path;
-use jvm::util::file_to_bytes;
-use jvm::klass_parser::KlassParser;
-
 fn main() {
-    let path = "testdata/HelloWorld.class".clone();
-    let bytes = file_to_bytes(Path::new(path))
-        .expect(&format!("Problem reading {}", path));
 
-
-    let mut parser = KlassParser::new(bytes);
-    parser.parse();
 }
 
 
