0014088857b0948e711f6593ebda07e8d40d6c92
Phodal Huang <h@phodal.com>
2020-07-12 10:07:31 +0800

    feat: make trait obejct works

--- src/classpath/classpath.rs
+++ src/classpath/classpath.rs
@@ -1,23 +1,25 @@
-// pub trait Entry {
-//   // className: fully/qualified/ClassName.class
-//   fn read_class(class_name: String);
-// }
-
-pub struct Entry {}
+pub trait Entry {
+  // className: fully/qualified/ClassName.class
+  fn read_class(&self, class_name: String);
+}
 
 pub struct ClassPath {
-    entries: Vec<Entry>
+    pub entries: Vec<Box<dyn Entry>>,
+    pub java_home: String,
+    pub user_path: String
 }
 
 impl ClassPath {
-    pub fn new() -> ClassPath {
+    pub fn new(java_home: String, user_path: String) -> ClassPath {
         ClassPath {
-            entries: vec![]
+            entries: vec![],
+            java_home,
+            user_path
         }
     }
 
-    pub fn parse() {
-        let path = ClassPath::new();
+    pub fn parse(java_home: String, user_path: String) {
+        let path = ClassPath::new(java_home, user_path);
         path.parse_boot_path();
         path.parse_ext_path();
         path.parse_user_class_path();
@@ -26,4 +28,17 @@ impl ClassPath {
     pub fn parse_boot_path(&self) {}
     pub fn parse_ext_path(&self) {}
     pub fn parse_user_class_path(&self) {}
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::classpath::classpath::ClassPath;
+
+    #[test]
+    fn test_load_class() {
+        let java_home: String = String::from("/Library/Java/JavaVirtualMachines/jdk1.8.0_202.jdk/Contents/Home/jre");
+        let user_path: String = String::from("testdata/java8");
+        let class_parse = ClassPath::parse(java_home, user_path);
+    }
 }
