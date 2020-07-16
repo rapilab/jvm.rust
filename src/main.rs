b58b39ef14897f4acf49fee2c8e685236c175517
Phodal Huang <h@phodal.com>
2020-07-16 13:59:22 +0800

    refactor: thinking in lifecycle

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,18 @@
-fn main() {}
+struct C;
+
+struct B<'b> {
+    c: &'b C,
+}
+
+struct A<'a> {
+    b: B<'a>,
+    c: &'a C, // now this is a reference too
+}
+
+fn main() {
+    let c1 = C;
+    let _ = A {
+        c: &c1,
+        b: B { c: &c1 },
+    };
+}
